dafdeb39aeff6791080aa3df3551883c7c7523bc
MEETNO1 <meetgupta1818@gmail.com>
2026-06-14 02:11:59 +0530

    Add files via upload

---
+++ leetcode.225.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+// #include<stack>
+// using namespace std;
+
+// public:
+//     void push(int x){
+//     queue<int>q;
+// }
+// My Stack() {}
+// void public (int x) {
+//     q.push(x);
+//     int sz = q.size();
+//     for(int i=0; i<sz-1;i++){
+//         q.push(q.front());
+//         q.pop();
+//     }
+// }
+// int pop() {
+//     int val = q.front();
+//     q.pop();
+//     return val;
+// }
+// int top() {
+//     return q.front();
+// }
+// bool empty() {
+//     return.empty();
+// }
